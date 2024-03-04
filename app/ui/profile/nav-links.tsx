354cba334e41b33d92d83cb1e67a2f348631d4d7
Karuna Solanki <karuna.solanki@outlook.com>
2024-03-04 11:21:54 +0100

    Navbar change according to theme

--- app/ui/profile/nav-links.tsx
+++ app/ui/profile/nav-links.tsx
@@ -1,17 +1,25 @@
-'use client';
+"use client";
 
-import { FingerPrintIcon, HomeIcon, DocumentIcon } from '@heroicons/react/24/outline';
+import {
+  FingerPrintIcon,
+  HomeIcon,
+  DocumentIcon,
+} from "@heroicons/react/24/outline";
 
-import Link from 'next/link';
-import clsx from 'clsx';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 
 const links = [
-  { name: 'Overview', href: '/profile', icon: HomeIcon },
-  { name: 'Session Details', href: '/profile/session-details', icon: DocumentIcon },
+  { name: "Overview", href: "/profile", icon: HomeIcon },
   {
-    name: 'Passkey List',
-    href: '/profile/passkey-list',
+    name: "Session Details",
+    href: "/profile/session-details",
+    icon: DocumentIcon,
+  },
+  {
+    name: "Passkey List",
+    href: "/profile/passkey-list",
     icon: FingerPrintIcon,
   },
 ] as const;
@@ -21,21 +29,28 @@ export default function NavLinks() {
 
   return (
     <>
-      {links.map(link => {
+      {links.map((link) => {
         const LinkIcon = link.icon;
         return (
           <Link
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                "bg-sky-100 text-blue-600": pathname === link.href,
+                "text-white": pathname !== link.href,
               },
             )}
+            style={{
+              backgroundColor:
+                pathname === link.href ? "#2C334B" : "transparent",
+            }}
           >
-            <LinkIcon className='w-6' />
-            <p className='hidden md:block'>{link.name}</p>
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block" style={{ fontSize: "18px" }}>
+              {link.name}
+            </p>
           </Link>
         );
       })}
