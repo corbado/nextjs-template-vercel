ed48f8057dc4a0d71f9ce1bcf94f9aca6871a163
Karuna Solanki <karuna.solanki@outlook.com>
2024-03-04 13:19:57 +0100

    Addition of copy icon in code snippet

--- app/profile/session-details/page.tsx
+++ app/profile/session-details/page.tsx
@@ -2,6 +2,7 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { nord } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
+import { FaCopy } from "react-icons/fa";
 
 export default function SessionDetails() {
   const cookieStore = cookies();
@@ -21,7 +22,12 @@ export default function SessionDetails() {
         className="mt-8 rounded-md bg-blue-500 p-6"
         style={{ backgroundColor: "#0C1A48" }}
       >
-        <div className="mb-3 mt-3">
+        <div className="relative mb-3 mt-3">
+          <FaCopy
+            className="absolute right-2 top-2"
+            style={{ color: "white" }}
+          />
+
           <SyntaxHighlighter language="json" style={nord}>
             {serializedDecodedShortSession}
           </SyntaxHighlighter>
