eb143434439afc0b489d01aecae59f1a0e0ec870
Karuna Solanki <karuna.solanki@outlook.com>
2024-03-04 11:50:25 +0100

    Signout button refactor, border and border height to distinguish navbar

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -7,7 +7,7 @@ export default function ProfileLayout({
 }) {
   return (
     <div className="relative z-0 flex flex flex min-h-screen flex-col items-center md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
+      <div className="h-full w-full flex-none md:w-64">
         <SideNav />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
--- app/ui/profile/sidenav.tsx
+++ app/ui/profile/sidenav.tsx
@@ -3,7 +3,7 @@ import SignoutButton from "./signout-button";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full grow flex-row justify-between space-x-2 border-r border-gray-600 px-3 md:flex-col md:space-x-0 md:space-y-2 md:px-2">
+    <div className="flex h-full grow flex-row justify-between space-x-2 border-r border-gray-600 px-3 md:flex-col md:space-x-0 md:space-y-2 md:px-2 md:pb-40">
       <NavLinks />
       <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
       <SignoutButton />
